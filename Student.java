12f071212da85a9cf72abd9ec8e41c417f61f5e1
Mnky7709 <861551829@qq.com>
2016-05-05 21:07:28 +0800

    '类的创建'

---
+++ Student.java
@@ -0,0 +1,25 @@
+package com.min.ch03;
+public class Student
+{
+	public String name;
+	private double weight;
+	public Student()
+		
+	{
+
+	}
+	public void sayhello()
+		
+	{
+		System.out.println(name+"say hello");
+	}
+	public int add(int a,int b)
+		
+	{
+		return a+b;
+	}
+
+
+
+
+}
---
+++ Test.java
@@ -0,0 +1,13 @@
+package com.min.ch03;
+
+public class Test
+{
+	public static void main(String[] args)
+		
+	{
+		Student stu;
+		stu=new Student();
+		stu.name="ÕÅÈı";
+		stu.sayhello();
+	}
+}
